d5ce0b2e087dfbba5d9078673194e3abfa8cb885
Juan Rubio <jua0729.jr@gmail.com>
2023-04-28 09:06:17 -0500

    hola mundo

---
+++ PrimerosPasos/src/primerospasos/PrimerosPasos.java
@@ -0,0 +1,12 @@
+package primerospasos;
+/**
+ * @author juan
+ */
+public class PrimerosPasos {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");     
+    }   
+}
